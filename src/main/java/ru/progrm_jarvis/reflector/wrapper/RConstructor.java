559d192a60dfe73f1638af21a040af08d2550f1a
PROgrm_JARvis <mrjarviscraft@gmail.com>
2018-06-16 13:38:17 +0300

    Add copyright info

--- src/main/java/ru/progrm_jarvis/reflector/wrapper/RConstructor.java
+++ src/main/java/ru/progrm_jarvis/reflector/wrapper/RConstructor.java
@@ -1,9 +1,28 @@
+/*
+ *  Copyright 2018 Petr P.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package ru.progrm_jarvis.reflector.wrapper;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.*;
 import ru.progrm_jarvis.reflector.AccessHelper;
 
 import java.lang.reflect.Constructor;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Wrapper for {@link Constructor} to be used with Reflector.
@@ -11,6 +30,8 @@ import java.lang.reflect.Constructor;
 @Value
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class RConstructor<T> implements ReflectorWrapper {
+
+    private static final Cache<Constructor<?>, RConstructor<?>> CACHE = CacheBuilder.newBuilder().weakValues().build();
 
     /**
      * Actual constructor wrapped.
@@ -22,8 +43,12 @@ public class RConstructor<T> implements ReflectorWrapper {
         return AccessHelper.accessAndGet(constructor, constructor -> constructor.newInstance(arguments));
     }
 
-    // not using lombok due to Intellij IDEA removing generic info sry
+    @SuppressWarnings("unchecked")
     public static <T> RConstructor<T> of(@NonNull final Constructor<T> constructor) {
-        return new RConstructor<>(constructor);
+        try {
+            return ((RConstructor<T>) CACHE.get(constructor, () -> new RConstructor<>(constructor)));
+        } catch (final ExecutionException e) {
+            throw new RuntimeException("Could not obtain RConstructor<T> value from cache");
+        }
     }
 }
--- src/main/java/ru/progrm_jarvis/reflector/wrapper/RField.java
+++ src/main/java/ru/progrm_jarvis/reflector/wrapper/RField.java
@@ -1,16 +1,38 @@
+/*
+ *  Copyright 2018 Petr P.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package ru.progrm_jarvis.reflector.wrapper;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.*;
 import org.jetbrains.annotations.Nullable;
 import ru.progrm_jarvis.reflector.AccessHelper;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Wrapper for {@link Field} to be used with Reflector.
  */
-@Value(staticConstructor = "of")
+@Value
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class RField<T> implements ReflectorWrapper {
+
+    private static final Cache<Field, RField<?>> CACHE = CacheBuilder.newBuilder().weakValues().build();
 
     /**
      * Actual field wrapped.
@@ -50,5 +72,14 @@ public class RField<T> implements ReflectorWrapper {
 
     public T updateValue(@Nullable final Object value) {
         return updateValue(null, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> RField<T> of(@NonNull final Field field) {
+        try {
+            return ((RField<T>) CACHE.get(field, () -> new RField<>(field)));
+        } catch (final ExecutionException e) {
+            throw new RuntimeException("Could not obtain RConstructor<T> value from cache");
+        }
     }
 }
