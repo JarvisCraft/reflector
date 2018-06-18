34f8f86f4ae3bded9bc37521effd973cff1be595
PROgrm_JARvis <mrjarviscraft@gmail.com>
2018-06-18 23:29:14 +0300

    Update deploy settings

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ ${TRAVIS_BRANCH} = 'master' ] && [ ${TRAVIS_PULL_REQUEST} == 'false' ]; then
     # Decrypt certificate
-    openssl aes-256-cbc -K ${encrypted_8a1cf5e3e25e_key} -iv ${encrypted_8a1cf5e3e25e_iv} \
+    openssl aes-256-cbc -K ${encrypted_24382952c2f5_key} -iv ${encrypted_24382952c2f5_iv} \
     -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     # Import decrypted certificate
     gpg --fast-import .travis/codesigning.asc
