f8eab8f530ba6a1b4e5d1da72c79919476d189ca
PROgrm_JARvis <mrjarviscraft@gmail.com>
2018-06-19 16:14:17 +0300

    Add debug to deploy script, return long groupId for further modules adding if needed

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-if [ ${TRAVIS_BRANCH} = 'master' ] && [ ${TRAVIS_PULL_REQUEST} == 'false' ]; then
+echo 'Executing deploy script for branch $TRAVIS_BRANCH (PR: $TRAVIS_PULL_REQUEST)'
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     # Decrypt certificate
+    echo 'Decrypting certificate'
     openssl aes-256-cbc -K ${encrypted_24382952c2f5_key} -iv ${encrypted_24382952c2f5_iv} \
     -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    echo 'Certificate has been successfully decrypted, importing it'
     # Import decrypted certificate
     gpg --fast-import .travis/codesigning.asc
+    echo 'Certificate has been successfully imported, deploying'
     # Deploy to OSSRH repository
     mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+    echo 'Deploy successful'
+else
+    echo 'Deploy conditions are not met'
 fi
